bde558c0893d801ffbd993b0fc30335d65e49dcf
BalaurZmeu <balaur.zmeu@gmail.com>
2024-08-07 18:32:16 +0300

    Added template for signup page

--- main/urls.py
+++ main/urls.py
@@ -9,33 +9,31 @@ urlpatterns = [
     path(
         '',
         TemplateView.as_view(template_name='main/home.html'),
-        name='home',
-    ),
+        name='home'),
     
-    path('signup/', views.SignupView.as_view(), name='signup'),
+    path(
+        'signup/',
+        views.SignupView.as_view(),
+        name='signup'),
     
     path(
         'about_us/',
         TemplateView.as_view(template_name='main/about_us.html'),
-        name='about_us',
-    ),
+        name='about_us'),
     
     path(
         'contact_us/',
         views.ContactUsView.as_view(),
-        name='contact_us',
-    ),
+        name='contact_us'),
     
     path(
         'products/<slug:tag>/',
         views.ProductListView.as_view(),
-        name='product_list',
-    ),
+        name='product_list'),
     
     path(
         'product/<slug:slug>/',
         DetailView.as_view(model=models.Product),
-        name='product',
-    ),
+        name='product'),
 ]
 
