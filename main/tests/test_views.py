1c1be8cf9e7f0960b1d8714e11f194f2770896f8
BalaurZmeu <balaur.zmeu@gmail.com>
2024-08-08 15:48:27 +0300

    Added tests for submit form and submit page

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from decimal import Decimal
 from main import forms, models
+from unittest.mock import patch
+from django.contrib import auth
 
 
 class TestPage(TestCase):
@@ -39,7 +41,7 @@ class TestPage(TestCase):
             price=Decimal('2.00'),
             active=False)
         response = self.client.get(
-            reverse('products', kwargs={'tag': 'all'}))
+            reverse('product_list', kwargs={'tag': 'all'}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'BookTime')
         
@@ -60,7 +62,7 @@ class TestPage(TestCase):
             slug='microsoft-win-guide',
             price=Decimal('12.00'))
         response = self.client.get(
-            reverse('products', kwargs={'tag': 'opensource'}))
+            reverse('product_list', kwargs={'tag': 'opensource'}))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'BookTime')
         
@@ -71,3 +73,34 @@ class TestPage(TestCase):
         self.assertEqual(
             list(response.context['object_list']),
             list(opensource_products))
+
+    def test_user_signup_page_loads_correctly(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signup.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(
+            response.context['form'], forms.UserCreationForm)
+    
+    def test_user_signup_page_submission_works(self):
+        post_data = {
+            'email': 'user@domain.com',
+            'password1': 'abcabcabc',
+            'password2': 'abcabcabc',
+        }
+        with patch.object(
+            forms.UserCreationForm, 'send_mail') as mock_send:
+                response = self.client.post(
+                    reverse('signup'), post_data)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            models.User.objects.filter(
+                email='user@domain.com'
+            ).exists()
+        )
+        self.assertTrue(
+            auth.get_user(self.client).is_authenticated
+        )
+        mock_send.assert_called_once()
+
