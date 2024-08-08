944ca9a5cd9f1da5f7be933b9c6c805ecb25f1e4
BalaurZmeu <balaur.zmeu@gmail.com>
2024-08-08 20:34:46 +0300

    Added views for list, create, update, delete address

--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.views.generic.edit import (
+    FormView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.views.generic.list import ListView
-from main import forms
-from main import models
+from main import forms, models
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 import logging
 
@@ -62,4 +68,54 @@ class SignupView(FormView):
         messages.info(self.request, 'You signed up successfully.')
     
         return response
+
+
+class AddressListView(LoginRequiredMixin, ListView):
+    model = models.Address
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressCreateView(LoginRequiredMixin, CreateView):
+    model = models.Address
+    fields = [
+        'name',
+        'address1',
+        'address2',
+        'zip_code',
+        'city',
+        'country',
+    ]
+    success_url = reverse_lazy('address_list')
+    
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Address
+    fields = [
+        'name',
+        'address1',
+        'address2',
+        'zip_code',
+        'city',
+        'country',
+    ]
+    success_url = reverse_lazy('address_list')
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Address
+    success_url = reverse_lazy('address_list')
+    
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
 
