0f1ae9f06bf608f05ecf164cc580ddc96807428d
BalaurZmeu <balaur.zmeu@gmail.com>
2024-08-24 14:42:21 +0300

    Working on admin.py to customize the admin site for different user groups

--- main/admin.py
+++ main/admin.py
@@ -223,3 +223,90 @@ class CentralOfficeOrderAdmin(admin.ModelAdmin):
         ),
     )
 
+
+class DispatchersOrderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'shipping_name',
+        'date_added',
+        'status',
+    )
+    list_filter = ('status', 'shipping_country', 'date_added')
+    inlines = (CentralOfficeOrderLineInline,)
+    fieldsets = (
+        ('Shipping info',
+            {'fields': (
+                'shipping_name',
+                'shipping_address1',
+                'shipping_address2',
+                'shipping_zip_code',
+                'shipping_city',
+                'shipping_country')
+            }
+        ),
+    )
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(status=models.Order.PAID)
+
+
+class ColoredAdminSite(admin.sites.AdminSite):
+    def each_context(self, request):
+        context = super().each_context(request)
+        context['site_header_color'] = getattr(
+            self, 'site_header_color', None
+        )
+        context['module_caption_color'] = getattr(
+            self, 'module_caption_color', None
+        )
+        return context
+
+
+class ReportingColoredAdminSite(ColoredAdminSite):
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                'orders_per_day/',
+                self.admin_view(self.orders_per_day),
+            )
+        ]
+        return my_urls + urls
+    
+    def orders_per_day(self, request):
+        starting_day = datetime.now() - timedelta(days=180)
+        order_data = (
+            models.Order.objects.filter(
+                date_added__gt = starting_day)
+            .annotate(day=TruncDay('date_added'))
+            .values('day')
+            .annotate(c=Count('id'))
+        )
+        labels = [
+            x['day'].strftime('%Y-%m-%d') for x in order_data
+        ]
+        values = [ x['c'] for x in order_data ]
+        
+        context = dict(
+            self.each_context(request),
+            title = 'Orders per day',
+            labels = labels,
+            values = values,
+        )
+        return TemplateResponse(
+            request, 'orders_per_day.html', context
+        )
+    
+    def index(self, request, extra_context=None):
+        reporting_pages = [
+            {
+                'name': 'Orders per day',
+                'link': 'orders_per_day/',
+            }
+        ]
+        if not extra_context:
+            extra_context = {}
+        extra_context = {'reporting_pages': reporting_pages}
+        return super().index(request, extra_context)
+
