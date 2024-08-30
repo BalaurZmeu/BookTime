0721163f39cf6bf1a320292fe1bfec9d3c9684ee
BalaurZmeu <balaur.zmeu@gmail.com>
2024-08-30 17:36:10 +0300

    Working on consumers.py

--- main/consumers.py
+++ main/consumers.py
@@ -36,4 +36,44 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         user_type = await database_sync_to_async(
             self.get_user_type
         )(self.scope['user'], self.order_id)
+        
+        if user_type == ChatConsumer.EMPLOYEE:
+            logger.info(
+                'Opening chat stream for employee %s',
+                self.scope['user'],
+            )
+            authorized = True
+        elif user_type == ChatConsumer.CLIENT:
+            logger.info(
+                'Opening chat stream for client %s',
+                self.scope['user'],
+            )
+            authorized = True
+        else:
+            logger.info(
+                'Unauthorized connection from %s',
+                self.scope['user'],
+            )
+            await self.close()
+        
+        if authorized:
+            self.r_conn = await aioredis.create_redis(
+                'redis://localhost'
+            )
+            await self.channel_layer.group_add(
+                self.room_group_name, self.channel_name
+            )
+            await self.accept()
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_join',
+                    'username': self.scope[
+                        'user'
+                    ].get_full_name(),
+                },
+            )
+
+    async def disconnect(self, close_code):
+        if not self.scope['user'].is_anonymous:
 
